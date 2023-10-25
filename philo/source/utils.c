6e45a5518ee46ed6dc8740aa0413268973c0039a
matheus.leonardo <matheusleotrab@gmail.com>
2023-10-25 00:39:16 -0300

    feat: create ATOI function

--- philo/source/utils.c
+++ philo/source/utils.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:04:12 by mleonard          #+#    #+#             */
-/*   Updated: 2023/10/18 23:13:16 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/10/24 23:37:07 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,21 @@ int	ft_isnb(char *str)
 		i++;
 	}
 	return (TRUE);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	unsigned long int	nb;
+
+	i = 0;
+	nb = 0;
+	while (str[i])
+	{
+		nb = (nb * 10) + (str[i] - '0');
+		i++;
+	}
+	if (nb > INT_MAX)
+		return (-1);
+	return ((int)(nb));
 }
