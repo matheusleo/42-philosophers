87a69e8fd1d2d5e3a074d2dd410f808234ea76c9
matheus.leonardo <matheusleotrab@gmail.com>
2023-10-25 00:40:06 -0300

    feat: parse the args + updadte the header to include the new functions and macros

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 23:12:49 by mleonard          #+#    #+#             */
-/*   Updated: 2023/10/18 23:36:12 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/10/25 00:29:05 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,37 @@
 
 # define TRUE 1
 # define FALSE 0
+# define INT_MAX 2147483647
+# define NB_PHILOS_POS 1
+# define TIME_TO_DIE_POS 2
+# define TIME_TO_EAT_POS 3
+# define TIME_TO_SLEEP_POS 4
+# define MIN_MEALS_POS 5
 # define MSG_USAGE "Use as:\n \
 philo <number_philosophers> <time_to_die> \
 <time_to_eat> <time_to_sleep> [minimum_meals]\n"
 # define ERR_NB_ARGS "Invalid number of arguments\n"
+# define ERRNO_NB_ARGS 1
 # define ERR_TYPE_ARGS "Invalid type of arguments\n"
+# define ERRNO_TYPE_ARGS 2
+# define NO_ERR 0
+
+typedef struct s_simulation
+{
+	unsigned int	nb_philo;
+	unsigned int	time_to_die;
+	unsigned int	time_to_eat;
+	unsigned int	time_to_sleep;
+	unsigned int	minimum_meals;
+}			t_sim;
 
 // Args
-void	validate_args(int argc, char *argv[]);
+int		validate_args(int argc, char *argv[]);
+t_sim	*parse_args(t_sim *simulation, int argc, char *argv[]);
 
 // Utility functions
 int		ft_isdigit(int c);
 int		ft_isnb(char *str);
+int		ft_atoi(char *str);
 
 #endif
--- philo/source/args.c
+++ philo/source/args.c
@@ -6,29 +6,40 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:25:30 by mleonard          #+#    #+#             */
-/*   Updated: 2023/10/18 23:34:52 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/10/25 00:36:56 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philosophers.h>
 
-void	validate_args(int argc, char *argv[])
+int	validate_args(int argc, char *argv[])
 {
 	int	i;
 
 	if (argc != 5 && argc != 6)
 	{
 		printf(ERR_NB_ARGS MSG_USAGE);
-		exit(1);
+		return (ERRNO_NB_ARGS);
 	}
 	i = 1;
 	while (i < argc)
 	{
-		if (!ft_isnb(argv[i]))
+		if (!ft_isnb(argv[i]) || ft_atoi(argv[i]) < 0)
 		{
 			printf(ERR_TYPE_ARGS MSG_USAGE);
-			exit(1);
+			return (ERRNO_TYPE_ARGS);
 		}
 		i++;
 	}
+	return (NO_ERR);
+}
+
+t_sim	*parse_args(t_sim *simulation, int argc, char *argv[])
+{
+	simulation->nb_philo = ft_atoi(argv[NB_PHILOS_POS]);
+	simulation->time_to_die = ft_atoi(argv[TIME_TO_DIE_POS]);
+	simulation->time_to_eat = ft_atoi(argv[TIME_TO_EAT_POS]);
+	simulation->time_to_sleep = ft_atoi(argv[TIME_TO_SLEEP_POS]);
+	simulation->minimum_meals = ft_atoi(argv[MIN_MEALS_POS]);
+	return (simulation);
 }
