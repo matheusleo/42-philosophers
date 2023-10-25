96f9c053e3ce5e194ac629b1c662192a2be31bf8
matheus.leonardo <matheusleotrab@gmail.com>
2023-10-25 00:40:26 -0300

    feat: parse and save the args to a variable

--- philo/source/main.c
+++ philo/source/main.c
@@ -6,13 +6,19 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 23:12:52 by mleonard          #+#    #+#             */
-/*   Updated: 2023/10/18 23:25:36 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/10/25 00:38:19 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philosophers.h>
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	validate_args(argc, argv);
+	t_sim simulation;
+	int err;
+
+	err = validate_args(argc, argv);
+	if (err)
+		return (err);
+	parse_args(&simulation, argc, argv);
 }
