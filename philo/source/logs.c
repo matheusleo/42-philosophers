984f9033996a36d5856fb558737ac6671eb1148d
matheus.leonardo <matheusleotrab@gmail.com>
2023-10-28 00:51:27 -0300

    fix: change log_status signature

--- philo/source/logs.c
+++ philo/source/logs.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 00:39:58 by mleonard          #+#    #+#             */
-/*   Updated: 2023/10/27 02:23:37 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/10/28 00:47:46 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,25 @@
 
 // TODO: use the enum instead of the macros
 // https://www.youtube.com/watch?v=lWzZ2l5n81c
-void	log_status(t_sim *sim, int status, int philo_id)
+void	log_status(t_philo *philo, int status)
 {
-	pthread_mutex_lock(&(sim->output_mutex));
+	int		philo_id;
+	int		should_print;
+	t_sim	*sim_config;
+
+	philo_id = philo->name;
+	sim_config = philo->sim_config;
+	should_print = !has_sim_stopped(sim_config);
+	pthread_mutex_lock(&(sim_config->output_mutex));
 	if (status == DEATH_S)
-		printf(DEATH_MSG, get_rel_timestamp(sim), philo_id + 1);
-	else if (status == EAT_S)
-		printf(EAT_MSG, get_rel_timestamp(sim), philo_id + 1);
-	else if (status == SLEEP_S)
-		printf(SLEEP_MSG, get_rel_timestamp(sim), philo_id + 1);
-	else if (status == THINK_S)
-		printf(THINK_MSG, get_rel_timestamp(sim), philo_id + 1);
-	else if (status == FORK_S)
-		printf(FORK_MSG, get_rel_timestamp(sim), philo_id + 1);
-	pthread_mutex_unlock(&(sim->output_mutex));
+		printf(DEATH_MSG, get_rel_timestamp(sim_config), philo_id + 1);
+	else if (status == EAT_S && should_print)
+		printf(EAT_MSG, get_rel_timestamp(sim_config), philo_id + 1);
+	else if (status == SLEEP_S && should_print)
+		printf(SLEEP_MSG, get_rel_timestamp(sim_config), philo_id + 1);
+	else if (status == THINK_S && should_print)
+		printf(THINK_MSG, get_rel_timestamp(sim_config), philo_id + 1);
+	else if (status == FORK_S && should_print)
+		printf(FORK_MSG, get_rel_timestamp(sim_config), philo_id + 1);
+	pthread_mutex_unlock(&(sim_config->output_mutex));
 }
