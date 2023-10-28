52acb6c6bcc60bff476ec0e841bb6ed863dfec80
matheus.leonardo <matheusleotrab@gmail.com>
2023-10-28 00:53:50 -0300

    feat: improve the monitor to also handle the meals

--- philo/source/monitor.c
+++ philo/source/monitor.c
@@ -6,18 +6,18 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 23:35:11 by mleonard          #+#    #+#             */
-/*   Updated: 2023/10/26 22:07:08 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/10/28 00:50:50 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philosophers.h>
 
-static int check_philos_deaths(t_sim *simulation)
+static int	check_philos_deaths(t_sim *simulation)
 {
-	size_t i;
-	unsigned long int rel_offset;
-	int	nb_philos;
-	t_philo	**philos;
+	size_t				i;
+	unsigned long int	rel_offset;
+	int					nb_philos;
+	t_philo				**philos;
 
 	i = 0;
 	nb_philos = simulation->nb_philo;
@@ -25,12 +25,13 @@ static int check_philos_deaths(t_sim *simulation)
 	while (i < nb_philos)
 	{
 		pthread_mutex_lock(&(philos[i]->last_meal_mutex));
-		rel_offset = get_rel_timestamp(philos[i]->sim_config) - philos[i]->last_meal;
-		if (rel_offset >= philos[i]->sim_config->time_to_die)
+		rel_offset = get_rel_timestamp(simulation) - philos[i]->last_meal;
+		if (rel_offset >= simulation->time_to_die)
 		{
-			simulation->should_stop = TRUE;
-			// TODO: create function to print the STATUS, TIMESTAMP and PHILO->NAME
-			printf("philo [%d] has died at %ldms\n", philos[i]->name, get_rel_timestamp(simulation));
+			pthread_mutex_lock(&(simulation->has_stopped_mutex));
+			simulation->has_stopped = TRUE;
+			pthread_mutex_unlock(&(simulation->has_stopped_mutex));
+			log_status(philos[i], DEATH_S);
 			pthread_mutex_unlock(&(philos[i]->last_meal_mutex));
 			return (++i);
 		}
@@ -40,16 +41,51 @@ static int check_philos_deaths(t_sim *simulation)
 	return (NO_ERR);
 }
 
-t_sim *monitor(t_sim *simulation)
+static int	check_philos_meals(t_sim *simulation)
 {
-	size_t i;
-	int has_philo_died;
+	size_t	i;
+	t_philo	**philos;
+
+	i = 0;
+	philos = simulation->philos;
+	while (i < simulation->nb_philo)
+	{
+		if (philos[i]->meal_count < simulation->minimum_meals)
+			return (FALSE);
+		i++;
+	}
+	pthread_mutex_lock(&(simulation->has_stopped_mutex));
+	simulation->has_stopped = TRUE;
+	pthread_mutex_unlock(&(simulation->has_stopped_mutex));
+	return (TRUE);
+}
+
+int	has_sim_stopped(t_sim *simulation)
+{
+	int	status;
+
+	pthread_mutex_lock(&(simulation->has_stopped_mutex));
+	status = simulation->has_stopped;
+	pthread_mutex_unlock(&(simulation->has_stopped_mutex));
+	return (status);
+}
+
+t_sim	*monitor(t_sim *simulation)
+{
+	int	has_philo_died;
+	int	min_meals_eaten;
 
 	while (TRUE)
 	{
 		has_philo_died = check_philos_deaths(simulation);
 		if (has_philo_died)
-			break;
+			break ;
+		if (simulation->minimum_meals > 0)
+		{
+			min_meals_eaten = check_philos_meals(simulation);
+			if (min_meals_eaten == TRUE)
+				break ;
+		}
 		usleep(1000);
 	}
 	return (simulation);
